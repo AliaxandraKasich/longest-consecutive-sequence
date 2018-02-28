a6c494a7192a9daa58377aa591be4f7e4726bf6f
AliaxandraKasich <Sane4ka022@mail.ru>
2018-02-28 23:31:16 +0300

    second commit

--- src/index.js
+++ src/index.js
@@ -1,11 +1,8 @@
 module.exports = function longestConsecutiveLength(array) {
-  function compareNumeric(a, b) {
-    if (a > b) return 1;
-    if (a < b) return -1;
-  }  
-  array.sort(compareNumeric);
-  var count1 = 0;
-  var count2 = 0;
+  
+  array.sort((x,y) => x-y);
+  var count1 = 1;
+  var count2 = 1;
   if( array.length === 0){
     return 0;
   }
@@ -13,15 +10,19 @@ module.exports = function longestConsecutiveLength(array) {
     return 1;
   }
   for(var i=0; i<array.length-1; i++){
+    if(array[i+1]-array[i] === 0){
+      continue;
+     }
     if(array[i+1]-array[i] === 1){
        count1++;
-      }
-    else{
-      if(count1>= count2){
+       if(count1 > count2){
         count2 = count1;
       }
-      count1 = 0;
-    }
+      }
+    else { count1 = 1;}
+
+    
+    
   }
-  return count2 + 1;
+  return count2 ;
 }
